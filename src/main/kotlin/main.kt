20a195690ea2351418cd3a752c8cc5e46e5d1362
matheuscarv69 <matheus9126@gmail.com>
2021-06-21 11:27:31 -0400

    :rocket: Initial Commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    println("Welcome to Bytebank")
+
+    val firstName: String
+    val lastName: String
+
+    firstName = "Matheus"
+    lastName = "Carvalho"
+
+    println(firstName + " " + lastName)
+
+    val n1 = 12
+    val n2 = 24
+    val amount = n1 + n2
+
+    println("Amount : " + amount)
+
+}
