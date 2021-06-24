5137f87e007f77baddef58a4410463168fa092a9
matheuscarv69 <matheus9126@gmail.com>
2021-06-24 10:47:14 -0400

    :sparkles: feat: using object expressions for client simulation

--- src/main/bytebank/main.kt
+++ src/main/bytebank/main.kt
@@ -1,7 +1,9 @@
+import core.InternalSystem
 import entities.account.CurrentAccount
 import entities.account.SavingsAccount
 import entities.account.totalAccount
 import entities.clients.Client
+import entities.employee.Authenticable
 
 fun main() {
 
@@ -9,10 +11,21 @@ fun main() {
     val matheusSavingsAccount = SavingsAccount(owner = matheus, numberAccount = 1000)
     val matheusCurrentAccount = CurrentAccount(owner = matheus, numberAccount = 1001)
 
-
     accountBehavioralTests()
 
     println("Total of accounts: $totalAccount")
+
+    val anonymousClient = object : Authenticable {
+        val name: String = "Anonymous"
+        val cpf: String = "123.321.213-32"
+        val passoword: Int = 1234
+
+        override fun autenticate(password: Int) = this.passoword == passoword
+    }
+
+    val internalSystem = InternalSystem()
+
+    internalSystem.entry(anonymousClient, 1234)
 
 
 }
