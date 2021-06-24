80a597c2c7a7d59e35533bb0d174a9d3c1eecf4b
matheuscarv69 <matheus9126@gmail.com>
2021-06-24 12:19:53 -0400

    :package: feat: extract counter accounts for package

---
+++ src/main/bytebank/functions/counterAccountTests.kt
@@ -0,0 +1,28 @@
+package functions
+
+import core.InternalSystem
+import entities.account.Account
+import entities.account.CurrentAccount
+import entities.account.SavingsAccount
+import entities.clients.Client
+import entities.employee.Authenticable
+
+fun CounterAccountsTests() {
+    val matheus = Client(name = "Matheus", "111.111.111-11", password = 1234)
+    val matheusSavingsAccount = SavingsAccount(owner = matheus, numberAccount = 1000)
+    val matheusCurrentAccount = CurrentAccount(owner = matheus, numberAccount = 1001)
+
+    println("Total of accounts: ${Account.totalAccounts}")
+
+    val anonymousClient = object : Authenticable {
+        val name: String = "Anonymous"
+        val cpf: String = "123.321.213-32"
+        val passoword: Int = 1234
+
+        override fun autenticate(password: Int) = this.passoword == passoword
+    }
+
+    val internalSystem = InternalSystem()
+
+    internalSystem.entry(anonymousClient, 1234)
+}
--- src/main/bytebank/main.kt
+++ src/main/bytebank/main.kt
@@ -1,31 +1,6 @@
-import core.InternalSystem
-import entities.account.Account
-import entities.account.CurrentAccount
-import entities.account.SavingsAccount
-import entities.clients.Client
-import entities.employee.Authenticable
-
 fun main() {
 
-    val matheus = Client(name = "Matheus", "111.111.111-11", password = 1234)
-    val matheusSavingsAccount = SavingsAccount(owner = matheus, numberAccount = 1000)
-    val matheusCurrentAccount = CurrentAccount(owner = matheus, numberAccount = 1001)
-
-    accountBehavioralTests()
-
-    println("Total of accounts: ${Account.totalAccounts}")
-
-    val anonymousClient = object : Authenticable {
-        val name: String = "Anonymous"
-        val cpf: String = "123.321.213-32"
-        val passoword: Int = 1234
-
-        override fun autenticate(password: Int) = this.passoword == passoword
-    }
-
-    val internalSystem = InternalSystem()
-
-    internalSystem.entry(anonymousClient, 1234)
 
 
 }
+
