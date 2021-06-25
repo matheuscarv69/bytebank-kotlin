77ae08bcdd760a81160dd231f0fae9ffce84b4cb
matheuscarv69 <matheus9126@gmail.com>
2021-06-25 10:43:34 -0400

    :sparkles: feat: using try expression

--- src/main/bytebank/main.kt
+++ src/main/bytebank/main.kt
@@ -1,7 +1,28 @@
+import java.lang.ArithmeticException
+
 fun main() {
-
-
+    println("início main")
+    funcao1()
+    println("fim main")
 }
 
+fun funcao1(){
+    println("início funcao1")
+    funcao2()
+    println("fim funcao1")
+}
 
+fun funcao2() {
+    println("início funcao2")
+    for (i in 1..5){
+        println(i)
+    }
 
+    try{
+        10/0
+    }catch (e: ArithmeticException){
+        println("Deu pau aqui $e.message")
+    }
+
+    println("fim funcao2")
+}
