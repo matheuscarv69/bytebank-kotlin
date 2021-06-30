edb7df4a6674700f5089466d8d6ffd8637ff60a6
matheuscarv69 <matheus9126@gmail.com>
2021-06-30 19:07:34 -0400

    :sparkles: feat: study run, apply, also and with

--- src/main/bytebank/main.kt
+++ src/main/bytebank/main.kt
@@ -1,6 +1,43 @@
+import entities.clients.Address
+
 fun main() {
 
+    val address = Address(
+        publicPlace = "Abel Monteiro Reis",
+        number = 1023
+    )
 
+    val address2: Address = Address()
+        .also { println("Generating a Address") }
+        .apply {
+            publicPlace = "Manoel Felipe"
+            number = 912
+            cep = "691123721"
+        }
+        .run {
+            publicPlace = publicPlace.uppercase()
+
+            if (cep.length > 8)
+                cep = "Invalid".uppercase()
+            println("CEP: $cep")
+
+
+            this // this because i need Address returns
+        }
+
+    println(address2)
+    println()
+
+    val myOldAddres: String = with(address2) {
+        "My old address is: $number,  $publicPlace Street, $cep"
+    }
+    println(myOldAddres)
+
+    println("\n")
+    println("With")
+    with(address) {
+        println("My address is: $number, $publicPlace Avenue")
+    }
 
 
 }
